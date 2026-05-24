7637da49fa99c97c25f29d48fc6a3287b4ce1a4d
TheLastt5 <mre_ylmz@outlook.com>
2026-05-24 21:50:09 +0300

    Create robot_turret.h

---
+++ Orinyum/robot_turret.h
@@ -0,0 +1,39 @@
+#ifndef ROBOT_TURRET_H
+#define ROBOT_TURRET_H
+
+#include "stm32f4xx_hal.h"
+#include <stdint.h>
+#include <stdbool.h>
+
+#define TURRET_PID_DT        0.020f
+#define TURRET_LOCK_THRESH   3.0f    
+#define TURRET_LOCK_TIME_MS  1000    
+#define TURRET_PWM_LIMIT     800U
+
+typedef struct {
+    float Kp;
+    float Ki;
+    float Kd;
+    float error_sum;
+    float last_error;
+} Turret_PID_t;
+
+typedef enum {
+    TURRET_SEARCHING = 0,
+    TURRET_TRACKING,
+    TURRET_LOCKED,
+    TURRET_FIRING
+} TurretState_t;
+
+extern TurretState_t turret_state;
+
+void  Turret_Init        (void);
+void  Turret_Update      (float pixel_err_pitch, float pixel_err_yaw,
+                           TIM_HandleTypeDef *htim_pitch, uint32_t ch_pitch,
+                           TIM_HandleTypeDef *htim_yaw,   uint32_t ch_yaw,
+                           TIM_HandleTypeDef *htim_laser, uint32_t ch_laser);
+void  Turret_Emergency_Stop(TIM_HandleTypeDef *htim_pitch, uint32_t ch_pitch,
+                             TIM_HandleTypeDef *htim_yaw,   uint32_t ch_yaw,
+                             TIM_HandleTypeDef *htim_laser, uint32_t ch_laser);
+
+#endif /* ROBOT_TURRET_H */
